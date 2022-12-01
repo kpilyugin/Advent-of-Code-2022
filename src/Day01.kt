c3d6f947ba91dfac1b715aceaafbce8b05526297
Kirill Pilyugin <kirill.pilyugin@delightex.com>
2022-12-01 11:20:57 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,27 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun groups(input: List<String>): List<Int> {
+        val groups = mutableListOf<Int>()
+        var current = 0
+        input.forEach {
+            if (it.isEmpty()) {
+                groups += current
+                current = 0
+            } else {
+                current += it.toInt()
+            }
+        }
+        groups += current
+        return groups
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = groups(input).max()
+
+    fun part2(input: List<String>) = groups(input).sortedDescending().take(3).sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
