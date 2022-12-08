27ff6da0d5c59d20558f72f99c71bac90c202d0c
Kirill Pilyugin <kirill.pilyugin@delightex.com>
2022-12-08 12:02:29 +0100

    Day 8

---
+++ src/Day08.kt
@@ -0,0 +1,81 @@
+import java.lang.Integer.max
+
+fun main() {
+    fun List<Int>.indicesOfVisible(): List<Int> {
+        return indices.filter { cur ->
+            indices.all {
+                it <= cur || get(it) < get(cur)
+            } || indices.all {
+                it >= cur || get(it) < get(cur)
+            }
+        }
+    }
+
+    fun part1(input: List<String>): Int {
+        val table = input.map { line -> line.map { it.digitToInt() } }
+        val n = table.size
+        val m = table[0].size
+        val d = Array(n) { IntArray(m) }
+
+        for (i in 0 until n) {
+            for (j in table[i].indicesOfVisible()) {
+                d[i][j] = 1
+            }
+        }
+        for (j in 0 until m) {
+            val column = (0 until n).map { table[it][j] }
+            for (i in column.indicesOfVisible()) {
+                d[i][j] = 1
+            }
+        }
+        return d.sumOf { it.sum() }
+    }
+
+    fun part2(input: List<String>): Int {
+        val table = input.map { line -> line.map { it.digitToInt() } }
+        val n = table.size
+        val m = table[0].size
+
+        var res = 0
+        for (i in 0 until n) {
+            for (j in 0 until m) {
+
+                fun canSee(dx: Int, dy: Int): Int {
+                    var x = i
+                    var y = j
+                    var visible = 0
+                    while (true) {
+                        val x1 = x + dx
+                        val y1 = y + dy
+                        if (x1 in 0 until n && y1 in 0 until m) {
+                            visible++
+                            if (table[x1][y1] < table[i][j]) {
+                                x = x1
+                                y = y1
+                            } else {
+                                return visible
+                            }
+                        } else {
+                            return visible
+                        }
+                    }
+                }
+
+                res = max(res,
+                    canSee(0, 1) * canSee(1, 0) * canSee(-1, 0) * canSee(0, -1)
+                )
+            }
+        }
+        return res
+    }
+
+    val testInput = readInputLines("Day08_test")
+    println(part1(testInput))
+    check(part1(testInput) == 21)
+    println(part2(testInput))
+    check(part2(testInput) == 8)
+
+    val input = readInputLines("Day08")
+    println(part1(input))
+    println(part2(input))
+}
