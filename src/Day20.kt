0b6ee8cf5ed51da2e67ec53203c7aea5a3997183
Kirill Pilyugin <kirill.pilyugin@delightex.com>
2022-12-20 19:46:01 +0100

    Day 20

---
+++ src/Day20.kt
@@ -0,0 +1,41 @@
+fun main() {
+    class PLong(val value: Long) {
+        override fun toString() = value.toString()
+    }
+
+    fun solve(input: List<String>, key: Long, repeatMix: Int): Long {
+        val initial = input.map { PLong(it.toLong() * key) }
+        val n = initial.size
+
+        val mixed = initial.toMutableList()
+        repeat(repeatMix) {
+            for (pValue in initial) {
+                var idx = mixed.indexOf(pValue)
+                idx += (pValue.value % (n - 1)).toInt()
+                while (idx < 0) idx += n - 1
+                while (idx >= n) idx -= (n - 1)
+                check(idx in 0 until n)
+                mixed.remove(pValue)
+                mixed.add(idx, pValue)
+            }
+        }
+
+        val zeros = initial.filter { it.value == 0L }
+        check(zeros.size == 1)
+        val idxZero = mixed.indexOf(zeros[0])
+        fun get(idx: Int): Long = mixed[(idx + idxZero) % n].value
+        return get(1000) + get(2000) + get(3000)
+    }
+
+    fun part1(input: List<String>) = solve(input, key = 1, repeatMix = 1)
+
+    fun part2(input: List<String>) = solve(input, key = 811589153, repeatMix = 10)
+
+    val testInput = readInputLines("Day20_test")
+    check(part1(testInput), 3L)
+    check(part2(testInput), 1623178306L)
+
+    val input = readInputLines("Day20")
+    println(part1(input))
+    println(part2(input))
+}
